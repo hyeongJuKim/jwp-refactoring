141eaa53b2879b39c5b40daa29eb1b8d997e15ef
HJ Kim <hjkim880527@gmail.com>
2022-12-22 15:20:26 +0900

    refactor: OrderTable, TableGroup 결합도 낮춤

--- kitchenpos-order/src/main/java/kitchenpos/ordertable/domain/OrderTable.java
+++ kitchenpos-order/src/main/java/kitchenpos/ordertable/domain/OrderTable.java
@@ -31,19 +31,19 @@ public class OrderTable {
         this.empty = empty;
     }
 
-    public OrderTable(Long id, NumberOfGuests numberOfGuests, boolean empty) {
-        this.id = id;
+    public void updateNumberOfGuest(NumberOfGuests numberOfGuests) {
+        validateShouldNotEmpty();
         this.numberOfGuests = numberOfGuests;
-        this.empty = empty;
     }
 
     public static OrderTable of(Long id, Long tableGroupId, int numberOfGuests, boolean empty) {
         return new OrderTable(id, tableGroupId, numberOfGuests, empty);
     }
 
-    public void updateNumberOfGuest(NumberOfGuests numberOfGuests) {
-        validateShouldNotEmpty();
+    public OrderTable(Long id, NumberOfGuests numberOfGuests, boolean empty) {
+        this.id = id;
         this.numberOfGuests = numberOfGuests;
+        this.empty = empty;
     }
 
     private void validateShouldNotEmpty() {
